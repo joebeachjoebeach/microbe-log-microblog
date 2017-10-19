5610cdf407be042a87d324b9bbce5cfcd8624da7
Joe Beach <joebeach@gmail.com>
2017-10-19 17:26:27 -0400

    fix silly bug

--- init_db.py
+++ init_db.py
@@ -1,7 +1,7 @@
 # from datetime import datetime
 from models import User, Post, db
 from app import app
-app.config.from_object('config')
+app.config.from_object('config-prod')
 
 with app.app_context():
     db.create_all()
